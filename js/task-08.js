19d210e67ead423bb3c639428227fe396c502981
borchuga <borchuga@gmail.com>
2020-09-16 19:26:38 +0300

    task-8 dev version

--- js/task-08.js
+++ js/task-08.js
@@ -5,7 +5,26 @@ const refs = {
 
 
 
-// const colorGenerator = () => {
-//   const randomColor = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
-//   return randomColor;
-// };
+function squareGenerator(number) { 
+    for (let i = 0; i < Numbers(number); i += 1) { 
+        const square = document.createElement('div');
+        square.style.width = `${squareSizeGenerator(number)}px`
+        square.style.height = `${squareSizeGenerator(number)}px`
+        square.style.backgroundColor = colorGenerator;
+        
+    }
+    
+}
+
+const colorGenerator = () => {
+  const randomColor = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+  return randomColor;
+};
+
+function squareSizeGenerator(number) { 
+    let squareSize = 30;
+    for (let i = 0; i < Numbers(number); i += 1) { 
+        squareSize = squareSize + (10 * i);
+    }
+    return squareSize;
+}
