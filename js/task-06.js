2eed2778997486a77a0cbc88b640a0b56e1ab962
Gennadiy <borchuga@gmail.com>
2020-09-18 00:29:45 +0300

    final version

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
-const inputRef = document.querySelector('#validation-input');
-inputRef.addEventListener('change', () => { 
-    if (inputRef.value.length == inputRef.dataset.length) {
-        inputRef.classList.remove('invalid'),
-            inputRef.classList.add('valid'),
-            console.log(inputRef);
-    } else inputRef.classList.add('invalid');
-    console.log(inputRef);
-})
+const inputRef = document.querySelector("#validation-input");
+inputRef.addEventListener("blur", () => {
+  if (inputRef.value.length == inputRef.dataset.length) {
+    inputRef.classList.remove("invalid"),
+      inputRef.classList.add("valid"),
+      console.log(inputRef);
+  } else inputRef.classList.add("invalid");
+  console.log(inputRef);
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,30 +1,42 @@
 const refs = {
-    inputWrapper: document.querySelector('#controls'),
-    boxWrapper: document.querySelector('#boxes')
-}
-
-
-
-function squareGenerator(number) { 
-    for (let i = 0; i < Numbers(number); i += 1) { 
-        const square = document.createElement('div');
-        square.style.width = `${squareSizeGenerator(number)}px`
-        square.style.height = `${squareSizeGenerator(number)}px`
-        square.style.backgroundColor = colorGenerator;
-        
-    }
-    
-}
+  inputWrapper: document.querySelector('#controls'),
+  boxWrapper: document.querySelector('#boxes'),
+  renderBtn: document.querySelector('button[data-action="render"]'),
+  destroy: document.querySelector('button[data-action="destroy"]'),
+  input: document.querySelector('input'),
+};
 
 const colorGenerator = () => {
   const randomColor = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
   return randomColor;
 };
 
-function squareSizeGenerator(number) { 
-    let squareSize = 30;
-    for (let i = 0; i < Numbers(number); i += 1) { 
-        squareSize = squareSize + (10 * i);
-    }
-    return squareSize;
-}
+const divSizeGenerator = number => {
+  let size = 30;
+  if (number == 1) {
+    return size;
+  }
+  return (size = 30 + 10 * (number - 1));
+};
+
+const divGenerator = number => {
+  const randomDiv = document.createElement('div');
+  randomDiv.style.backgroundColor = colorGenerator();
+  randomDiv.style.width = `${divSizeGenerator(number)}px`;
+  randomDiv.style.height = `${divSizeGenerator(number)}px`;
+  refs.boxWrapper.appendChild(randomDiv);
+};
+
+const divCreator = number => {
+  for (let i = 0; i < number; i += 1) {
+    divGenerator(i);
+  }
+};
+
+refs.renderBtn.addEventListener('click', () => {
+  divCreator(refs.input.value);
+});
+
+refs.destroy.addEventListener('click', () => {
+  refs.boxWrapper.innerHTML = '';
+});
